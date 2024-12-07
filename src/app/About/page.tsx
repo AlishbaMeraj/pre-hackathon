3f8d94268fc1c6d1f657ae0e447d69f676014864
AlishbaMeraj <alishbameraj961@gmail.com>
2024-12-07 22:31:47 +0500

    Initial commit: Upload Next.js project

---
+++ src/app/About/page.tsx
@@ -0,0 +1,17 @@
+import About from "../component/About"
+import Views from "../component/View"
+import Freedelvry from "../component/FreeDelivery"
+
+
+
+function AboutPage(){
+    return(
+       <div>
+         <About />
+         <Views />
+         <Freedelvry />
+         
+       </div>
+    )
+}
+export default AboutPage
